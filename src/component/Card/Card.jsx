bb82783561c1c692305ab98968f16dce7271a623
durotemy <durotemy@gmail.com>
2022-11-20 11:36:11 +0100

    responive carusel

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -24,7 +24,7 @@ const Card = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 5,
+          slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
           dots: true
